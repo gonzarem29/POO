8a6680b1a4320c26410920b902e2cb76d5379926
Gonzalo Gabriel Rementeria <127883905+gonzarem29@users.noreply.github.com>
2024-06-11 22:34:57 -0300

    Update main.cpp

--- Ejercicio 12/main.cpp
+++ Ejercicio 12/main.cpp
@@ -6,19 +6,15 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    // Crear un objeto QLabel
     QLabel label;
-
-    // Cargar la imagen desde un archivo JPG
     QPixmap image("C://Users//gonza//OneDrive//Imágenes//Capturas de pantalla//Screenshot 2024-01-13 111858.png");
     label.setPixmap(image);
-    label.setScaledContents(true);
-    // Mostrar el QLabel maximizado
     label.showMaximized();
+    label.setScaledContents(true);
 
-    // Configurar un temporizador para cerrar la aplicación después de 3 segundos
+
     QTimer::singleShot(3000, &a, SLOT(quit()));
 
     return a.exec();
 }
+
